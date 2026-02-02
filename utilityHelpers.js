56351bbd46398d1b41396130bff54aeaac45aa1b
peterbenoit <peterbenoit@gmail.com>
2026-02-02 09:48:15 -0500

    Add support for crypto.randomUUID in generateUUID method

--- utilityHelpers.js
+++ utilityHelpers.js
@@ -118,6 +118,10 @@ class UtilityHelpers {
 	}
 
 	static generateUUID() {
+		if (typeof crypto !== "undefined" && typeof crypto.randomUUID === "function") {
+			return crypto.randomUUID();
+		}
+
 		return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
 			const r = (Math.random() * 16) | 0;
 			const v = c === "x" ? r : (r & 0x3) | 0x8;
