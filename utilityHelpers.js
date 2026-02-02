3e8795f4280e0d5ce5ea222eb5d99b9878617f07
peterbenoit <peterbenoit@gmail.com>
2026-02-02 09:45:39 -0500

    Add URL.revokeObjectURL to release object URL after download

--- utilityHelpers.js
+++ utilityHelpers.js
@@ -644,6 +644,7 @@ class UtilityHelpers {
 		document.body.appendChild(link);
 		link.click();
 		document.body.removeChild(link);
+		URL.revokeObjectURL(link.href);
 	}
 
 	// Go fullscreen with the document or a specific element
